ade31e324ad077d07e0031b0fb3d40828d2520a7
Romel180149 <mosiurrahmanromel180149@gmail.com>
2023-09-24 22:08:50 +0600

    json data and navbar added

---
+++ src/components/Header/Navbar/Logo.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Logo = () => {
+    return (
+        <div>
+            
+            <h1 className='text-xl font-semibold'>Donation Campaign</h1>
+        </div>
+    );
+};
+
+export default Logo;
